0c48ebf16500fc529838e9ba9ff46ea9dfb4fab8
Daniel Khusainov <dfkhusainov@edu.hse.ru>
2026-06-15 13:32:19 +0300

    Excluded 'country_full' column from datasets

--- ml_utils.py
+++ ml_utils.py
@@ -5,7 +5,9 @@ from datetime import datetime
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
+
 RESULTS_FILE = 'experiment_results.parquet'
+
 
 def load_results():
     try:
@@ -13,8 +15,10 @@ def load_results():
     except FileNotFoundError:
         return pd.DataFrame(columns=['timestamp', 'method', 'params', 'score', 'duration_sec'])
 
+
 def save_results(df):
     df.to_parquet(RESULTS_FILE, index=False)
+
 
 def run_experiment(pipeline, method_name, param_dict=None):
     """
@@ -22,19 +26,18 @@ def run_experiment(pipeline, method_name, param_dict=None):
     """
     if param_dict is None:
         param_dict = {}
-    
+
     start_time = time.time()
     timestamp = datetime.now().isoformat()
-    
+
     df = pd.read_parquet('main_df_edited.parquet')
-    X = df.drop(columns=['reservation_status', 'reservation_status_date', 'is_canceled']).copy()
+    X = df.drop(columns=['reservation_status', 'reservation_status_date', 'is_canceled', 'country_full']).copy()
     y = df['is_canceled']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=22)
     pipeline.fit(X_train, y_train)
     duration = time.time() - start_time
     score = f1_score(y_test, pipeline.predict(X_test))
-    
-    
+
     result_row = {
         'timestamp': timestamp,
         'method': method_name,
@@ -42,11 +45,11 @@ def run_experiment(pipeline, method_name, param_dict=None):
         'score': score,
         'duration_sec': duration,
     }
-    
+
     results_df = load_results()
     results_df = pd.concat([results_df, pd.DataFrame([result_row])], ignore_index=True)
     save_results(results_df)
-    
+
     print(f"{method_name} | score: {score:.5f} time: {duration:.1f}s")
     return result_row
 
